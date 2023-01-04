bcda74e082a79bc05bfb368817a44d8e90ec5e54
Blaise Saunders <21285703+DavidoRotho@users.noreply.github.com>
2023-01-04 16:17:40 +1100

    wip: add picture scraper code

---
+++ scrapeGenPics.py
@@ -0,0 +1,41 @@
+PEXELS_API_KEY = '563492ad6f91700001000001c28844238cb4477c888876d50d876565' # Generic API key from tutorial dont roast me fo this 1 i doing u a favor homie
+
+import requests
+import json
+from pprint import pprint
+from requests.structures import CaseInsensitiveDict
+from operator import itemgetter
+import os
+import sys
+import subprocess
+import requests
+
+query = sys.argv[1]
+
+def getPage(url):
+    print("Scraping: "+url)
+    headers = CaseInsensitiveDict()
+    headers["Authorization"] = "563492ad6f91700001000001f3cb9451a6984a4e9714c7c5fbaa3a87"
+    resp = requests.get(url, headers=headers)
+    print("Status: "+str(resp.status_code))
+    
+    return json.loads(resp.text)
+
+url = "https://api.pexels.com/v1/search?query="+query+"&per_page=128"
+jason = getPage(url)
+
+while jason['next_page']:
+    for pic in jason['photos']:
+        if float(pic['width']) > 1920 and float(pic['height']) > 1080:
+            url = pic['src']['original']
+            print("Getting: "+url)
+            img_data = requests.get(url).content
+            with open(str(pic['id'])+'.jpg', 'wb') as handler:
+                handler.write(img_data)
+    jason = getPage(jason['next_page'])
+        
+
+"""
+stream = os.popen("./combine.sh "+query)
+output = stream.read()
+"""
